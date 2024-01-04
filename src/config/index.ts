093834ffb40075152fe19250afdd148dba7798e9
Robin Grass <hej@carbin.se>
2024-01-04 19:28:15 +0100

    feat: project configuration file

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,12 @@
 import $pkg from '../../package.json';
+import $prj from '../../project.json';
 
 export const pkg = {
 	name: $pkg.name,
 	version: $pkg.version,
+};
+
+export const project = {
+	name: $prj.name,
+	version: $prj.version,
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { project } from 'config';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
@@ -7,6 +8,9 @@ import './styles.css';
 
 // Do not remove the bang (!), it's a non-null assertion operator.
 const mountNode: HTMLElement = document.getElementById('root')!;
+
+// Change this if you want, but keep it in project
+document.title = `${project.name} - v${project.version}`;
 
 createRoot(mountNode).render(
 	<StrictMode>
